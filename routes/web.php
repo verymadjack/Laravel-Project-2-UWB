0a7e4aed2400bc1088c44d730b1ab31a240420ba
verymadjack <verymadjacek@gmail.com>
2022-11-15 00:33:11 +0200

    Full Project 2

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::group(['middleware' => ['is_admin']], function() {
+        Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+        Route::resource('posts', \App\Http\Controllers\PostController::class);
+    });
+
+});
+
+
+require __DIR__.'/auth.php';
